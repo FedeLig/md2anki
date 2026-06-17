21cefe775b99b85906e94d567ff4ed8947501a51
FedeLig <111727921+FedeLig@users.noreply.github.com>
2026-06-17 19:54:09 +0200

    Delete md2anki directory

--- md2anki/src/qwen_vl_setup.sh
+++
@@ -1,6 +0,0 @@
-python -m llama_cpp.server \
-  --model ./models/qwen2-vl-7b-instruct-q4_k_m.gguf \
-  --clip_model_path ./models/mmproj-qwen2-vl-7b-instruct-f16.gguf \
-  --chat_format chatml \
-  --n_ctx 4096 \
-  --port 8000
